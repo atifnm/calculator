c59312299503389607cd59048fe921b1f308423c
Atif Naeem <atif.nm@gmail.com>
2020-07-04 13:01:13 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function getnumber(num){
+    var result = document.getElementById("result");
+    result.value += num
+}
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value =""
+}
+function getResult(){
+    var result = document.getElementById("result");
+    result.value= eval(result.value)
+}
+function back(){
+    var result = document.getElementById("result");
+    result.value = result.value.slice(0,-1)
+}
